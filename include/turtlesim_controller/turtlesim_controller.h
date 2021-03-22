63010f01a07d6d8300b0c19548f154825fe5beb2
Ryosuke Takizawa <ryosuke.takizawa.25@gmail.com>
2021-03-22 14:54:03 +0900

    add square mode

--- include/turtlesim_controller/turtlesim_controller.h
+++ include/turtlesim_controller/turtlesim_controller.h
@@ -15,18 +15,26 @@ public:
 private:
     // method
     void pose_callback(const turtlesim::PoseConstPtr&);
+    void record_reset();
     geometry_msgs::Twist draw_circle();
+    geometry_msgs::Twist draw_square();
 
     // parameter
     ros::NodeHandle private_nh;
     double hz;
     std::string mode;
+    double value_x;
+    double value_z;
+
+    double square_length;
 
     // member
     ros::NodeHandle nh;
     ros::Publisher cmd_vel_pub;
     ros::Subscriber pose_sub;
     turtlesim::Pose current_pose;
+    double record_distance;
+    double record_orientation;
 };
 
 #endif // _TURTLESIM_CONTROLLER_H_
--- src/turtlesim_controller.cpp
+++ src/turtlesim_controller.cpp
@@ -5,6 +5,13 @@ TurtlesimController::TurtlesimController() : private_nh("~")
     // parameter
     private_nh.param("hz", hz, {10});
     private_nh.param("mode", mode, {"circle"});
+    private_nh.param("value_x", value_x, {1.0});
+    private_nh.param("value_z", value_z, {1.0});
+    private_nh.param("square_length", square_length, {1.0});
+
+    //initialize
+    record_distance = 0;
+    record_orientation = 0;
 
     // subscriber
     pose_sub = nh.subscribe("/turtle1/pose", 1, &TurtlesimController::pose_callback, this);
@@ -15,21 +22,71 @@ TurtlesimController::TurtlesimController() : private_nh("~")
     // print parameter
     std::cout << "=== turtlesim_controller ===" << std::endl;
     std::cout << "hz: " << hz << std::endl;
+    std::cout << "mode: " << mode << std::endl;
+    std::cout << "value_x: " << value_x << std::endl;
+    std::cout << "value_z: " << value_z << std::endl;
+
+    if (mode == "square")
+    {
+        std::cout << "square_length: " << square_length << std::endl;
+    }
+
     std::cout << std::endl;
 }
 
 void TurtlesimController::pose_callback(const turtlesim::PoseConstPtr &msg)
 {
     current_pose = *msg;
-    std::cout << "current_pose:( " << current_pose.x << ", " << current_pose.y << ", " << current_pose.theta << ")" << std::endl;
+    //std::cout << "current_pose:( " << current_pose.x << ", " << current_pose.y << ", " << current_pose.theta << ")" << std::endl;
+}
+
+void TurtlesimController::record_reset()
+{
+    record_distance = 0;
+    record_orientation = 0;
 }
 
 geometry_msgs::Twist TurtlesimController::draw_circle()
 {
     geometry_msgs::Twist twist;
-    twist.linear.x = 1.0;
-    twist.angular.z = 1.0;
+    twist.linear.x = value_x;
+    twist.angular.z = value_z;
 
+    return twist;
+}
+
+geometry_msgs::Twist TurtlesimController::draw_square()
+{
+    if(current_pose.theta < 0)
+    {
+        current_pose.theta += 2.0 * M_PI;
+    }
+    static turtlesim::Pose previous_pose = current_pose;
+
+    record_distance += sqrt(pow(current_pose.x - previous_pose.x, 2) + pow(current_pose.y - previous_pose.y, 2));
+    record_orientation += current_pose.theta - previous_pose.theta;
+
+    geometry_msgs::Twist twist;
+    if(abs(record_distance) <= square_length)
+    {
+        std::cout << "[Edge] ( " << record_distance << ", " << record_orientation << ")" << std::endl;
+        twist.linear.x = value_x;
+        twist.angular.z = 0.0;
+    }
+    else if(abs(record_orientation) <= M_PI /2)
+    {
+        std::cout << "[Node] ( " << record_distance << ", " << record_orientation << ")" << std::endl;
+        twist.linear.x = 0.0;
+        twist.angular.z = value_z;
+    }
+    else
+    {
+        record_reset();
+        std::cout << std::endl;
+        std::cout << "===== Let's go Next node!!!!! =====" << std::endl;
+    }
+
+    previous_pose = current_pose;
     return twist;
 }
 
@@ -40,10 +97,16 @@ void TurtlesimController::process()
 
     while(ros::ok())
     {
+        ros::spinOnce();
+
         geometry_msgs::Twist twist;
         if(mode == "circle")
         {
             twist = draw_circle();
+        }
+        else if(mode == "square")
+        {
+            twist = draw_square();
         }
         else
         {
@@ -56,10 +119,9 @@ void TurtlesimController::process()
         //std::cout << "linear.x:  " << twist.linear.x << std::endl;
         //std::cout << "angular.z: " << twist.angular.z << std::endl;
 
-        ros::spinOnce();
         loop_rate.sleep();
 
-        if(ros::Time::now() - start > ros::Duration(10))
+        if(ros::Time::now() - start > ros::Duration(60))
             break;
     }
 }
@@ -68,6 +130,9 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "turtlesim_controller");
     TurtlesimController turtlesim_controller;
+
+    ros::Duration delay(0.5);
+    delay.sleep();
     turtlesim_controller.process();
 
     return 0;
